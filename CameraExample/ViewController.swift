8fa0f161de5dae148fd982977fdd4f53023cfff2
Jason Carucci <jcarucci@gmail.com>
2020-08-15 15:07:41 -0400

    Identify with SqueezeNet model

--- CameraExample/ViewController.swift
+++ CameraExample/ViewController.swift
@@ -6,7 +6,9 @@
 //  Copyright © 2020 Grand Wazoo. All rights reserved.
 //
 
+import AVKit
 import UIKit
+import Vision
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
@@ -24,5 +26,32 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         vc.allowsEditing = true
         vc.delegate = self
         present(vc, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+
+        guard let image = info[.editedImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+
+        // print out the image size as a test
+        print(image.size)
+        analyzeImage(image.cgImage!)
+    }
+    
+    func analyzeImage(_ img: CGImage) {
+        guard let model1 = try? VNCoreMLModel(for: SqueezeNet().model) else { return }
+        //guard let model2 = try? VNCoreMLModel(for: Resnet50().model) else { return }
+        let request = VNCoreMLRequest(model: model1) { [unowned self] (finishedReq, err) in
+            DispatchQueue.main.async {
+                guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
+                
+                guard let f = results.first else { return }
+                print("\(f.identifier) - Confidence: \(f.confidence)")
+            }
+        }
+        try? VNImageRequestHandler(cgImage: img, options: [:]).perform([request])
     }
 }
